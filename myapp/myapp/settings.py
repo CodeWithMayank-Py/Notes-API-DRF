261271359d6722e776f2fd9a4891ff6b6fccf902
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-10-17 19:03:00 +0530

    Configuring Project

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django Rest Framework
+    'rest_framework',
     # Custom Apps
     'notes.apps.NotesConfig',
 ]
